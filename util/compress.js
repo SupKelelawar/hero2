433550552dc3be19226062cd34349c5e81711f97
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-11-09 23:03:28 +0700

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -1,11 +1,18 @@
 // Compresses an image using Sharp library
 const sharp = require("sharp");
 
-function compress(imagePath, useWebp, grayscale, quality, originalSize) {
+/**
+ * Mengompres gambar dengan pengaturan lebar dan kualitas
+ * @param {Buffer} imagePath - Path gambar asli
+ * @param {boolean} useJPEG - Format gambar (true untuk WebP, false untuk JPEG)
+ * @param {number} quality - Kualitas gambar (default: 80)
+ * @param {number} originalSize - Ukuran asli gambar
+ */
+function compress(imagePath, useWebp, width = 300, quality = 80, originalSize) {
   let format = useWebp ? "webp" : "jpeg";
 
   return sharp(imagePath)
-    .grayscale(grayscale)
+    .resize(width) // Mengatur lebar gambar menjadi 300
     .toFormat(format, { quality, progressive: true, optimizeScans: true })
     .toBuffer({ resolveWithObject: true })
     .then(({ data, info }) => ({
