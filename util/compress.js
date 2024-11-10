a231687ffc67cd2ec158f002fb84ffdbc7f29aa8
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-11-10 10:44:26 +0700

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -11,8 +11,12 @@ function compress(data, width = 300, quality = 85, originalSize) {
   let format = "jpeg"; // Set format to jpeg only
 
   return sharp(data)
-    .resize(width, null, { withoutEnlargement: true }) // Menjaga rasio dan mencegah perbesaran gambar
-    .toFormat(format, { quality }) // Menyederhanakan opsi kompresi
+    .resize(width, null, {
+      withoutEnlargement: true,  // Jangan membesarkan gambar
+      kernel: sharp.kernel.lanczos3, // Menggunakan kernel Lanczos untuk kualitas tinggi
+      // Lanczos3 adalah algoritma resampling yang bagus untuk mempertahankan kualitas gambar saat resize
+    })
+    .toFormat(format, { quality })  // Atur kualitas kompresi
     .toBuffer({ resolveWithObject: true })
     .then(({ data, info }) => ({
       err: null,
